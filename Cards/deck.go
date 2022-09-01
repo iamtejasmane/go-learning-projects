4c5ffc91964f50f7247de27e6b8286cb059ac5c3
Tejas Mane <tejasmane485@gmail.com>
2022-09-01 18:34:29 +0530

    write to file feature

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // which is a sloce of strings
@@ -28,4 +32,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) {
+	ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,11 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
+	// cards.print()
+
 }
 
 func newCard() string {
