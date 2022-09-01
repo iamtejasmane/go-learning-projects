6abe89b277f90cc68251c6387430fb20bf663fac
Tejas Mane <tejasmane485@gmail.com>
2022-09-01 16:20:41 +0530

    newcards method added

--- Cards/deck.go
+++ Cards/deck.go
@@ -6,6 +6,20 @@ import "fmt"
 // which is a sloce of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubes"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of"+suite)
+		}
+	}
+
+	return cards
+}
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- Cards/main.go
+++ Cards/main.go
@@ -1,30 +1,10 @@
 package main
 
-// func main() {
-// 	card := newCard()
-
-// 	fmt.Println(card)
-// }
+func main() {
+	cards := newDeck()
+	cards.print()
+}
 
 func newCard() string {
 	return "Five of Diamonds"
 }
-
-// Slice
-func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	cards.print()
-}
-
-// type book string
-
-// func (b book) printBook() {
-// 	fmt.Println(b)
-// }
-
-// func main() {
-// 	var b book = "Harry Potter"
-// 	b.printBook()
-// }
